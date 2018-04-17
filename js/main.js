c30a0c3720b0099d8522d21920e03b0aef81938e
Windows-Roy <roypers89@gmail.com>
2018-04-17 10:30:56 +0200

    fix - start - reponsiv meny

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+
+    $(".burger-nav").on("click", function(){
+       console.log("click");
+       
+        $("header nav ul").toggleClass("open");
+    });
+    
+});
